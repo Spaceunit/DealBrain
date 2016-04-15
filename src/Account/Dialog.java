6126884ff33c050c9c20a083cf94b299dfb4561f
Misha <mishaukr22@gmail.com>
2016-04-15 21:32:13 +0300

    DialogList

---
+++ src/Account/Dialog.java
@@ -0,0 +1,31 @@
+package Account;
+
+/**
+ * Created by Misha on 15.04.2016.
+ */
+public class Dialog {
+    private int membersID;
+    private int owner;
+
+    public void setMembers(int members) {
+        this.membersID = members;
+    }
+
+    public int getMembers() {
+        return membersID;
+    }
+
+    public void setOwner(int owner) {
+        this.owner = owner;
+    }
+
+    public int getOwner() {
+        return owner;
+    }
+    public addMembers(){
+
+    }
+    public removeMembers(){
+
+    }
+}
