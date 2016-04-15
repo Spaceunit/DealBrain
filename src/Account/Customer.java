512cf67822fc329bf46a9c9900a6fb5de747a9ed
Nazotron1923 <kaminskyi.nazar.1923@gmail.com>
2016-04-15 21:19:28 +0300

    New

--- src/Account/Customer.java
+++ src/Account/Customer.java
@@ -4,4 +4,35 @@ package Account;
  * Created by lenovo on 15.04.2016.
  */
 public class Customer {
+
+    private int amountGoods;
+    private boolean methodPayment;
+    private boolean signatureDeal;
+
+    public void setAmountGoods(int amountGoods) {
+        this.amountGoods = amountGoods;
+    }
+
+    public int getAmountGoods() {
+        return amountGoods;
+    }
+
+    public void setSignatureDeal(boolean signatureDeal) {
+        this.signatureDeal = signatureDeal;
+    }
+
+    public boolean isSignatureDeal() {
+        return signatureDeal;
+    }
+
+
+
+    public void setMethodPayment(boolean methodPayment) {
+        this.methodPayment = methodPayment;
+    }
+
+    public boolean isMethodPayment() {
+        return methodPayment;
+    }
+
 }
