a2b1c117e5d6d246c967ac2fb5d2d2bb8ec9d0ad
yaroslav <yaroslav.mavliutov@gmail.com>
2016-04-15 21:32:13 +0300

    class General_ID

---
+++ src/Account/General_ID.java
@@ -0,0 +1,11 @@
+package Account;
+
+/**
+ * Created by Yaroslav on 15.04.2016.
+ */
+public class General_ID {
+    private long id;
+
+    public void SetID(long id ) {this.id = id;}
+    public long GetID() {return id;}
+}
