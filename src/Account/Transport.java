77ec1812e5b7130674b067ba10812250832fcfd0
Vavadimir <freesoulvova@gmail.com>
2016-04-15 21:34:33 +0300

    var

--- src/Account/Transport.java
+++ src/Account/Transport.java
@@ -7,6 +7,8 @@ public class Transport {
     private int capacityCargo;
     private boolean accessTransit;
     private String model;
+    private int[] typeCab;
+    //This array consist of some numbers, that means certain type of car's cab
 
 }
 
