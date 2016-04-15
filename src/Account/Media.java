fbf2ec1b04f247573734635a98e928bf352674c8
Misha <mishaukr22@gmail.com>
2016-04-15 22:03:19 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/Account/Media.java
@@ -0,0 +1,35 @@
+package Account;
+
+/**
+ * Created by Misha on 15.04.2016.
+ */
+public class Media {
+    private String mediaID;
+    private String videoID;
+    private String audioID;
+
+    public void setAudioID(String audioID) {
+        this.audioID = audioID;
+    }
+
+    public void setMediaID(String mediaID) {
+        this.mediaID = mediaID;
+    }
+
+    public String getMediaID() {
+        return mediaID;
+    }
+
+    public String getAudioID() {
+        return audioID;
+    }
+
+    public void setVideoID(String videoID) {
+        this.videoID = videoID;
+    }
+
+    public String getVideoID() {
+        return videoID;
+    }
+
+}
