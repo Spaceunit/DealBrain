71e3c4e853dbffe9b5ce1d5a03298dc7be21af79
Oleksiy <oleksiypolshchak@gmail.com>
2016-04-16 21:25:35 +0300

    changes-in-class-ID-package-Account

--- src/Account/ID.java
+++ src/Account/ID.java
@@ -4,4 +4,13 @@ package Account;
  * Created by oleksiypolshchak on 4/15/16.
  */
 public class ID {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
