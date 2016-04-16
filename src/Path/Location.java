3a83a33423368f88b64cd0bf0b273ae7bbb78dce
Vavadimir <freesoulvova@gmail.com>
2016-04-16 21:01:12 +0300

    var and methods

---
+++ src/Path/Location.java
@@ -0,0 +1,16 @@
+package Path;
+
+/**
+ * Created by User on 4/16/2016.
+ */
+public class Location {
+    private int coordinates;
+    private int radiusofreach;
+
+    public int getCoordinates(){
+        return coordinates;
+    }
+    public int getRadiusofreach(){
+        return radiusofreach;
+    }
+}
