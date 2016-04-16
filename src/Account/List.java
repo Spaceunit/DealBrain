08cb9d359e714508919462b91b54f972d0539066
Misha <mishaukr22@gmail.com>
2016-04-16 14:26:13 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/Account/List.java
@@ -0,0 +1,17 @@
+package Account;
+
+/**
+ * Created by Misha on 16.04.2016.
+ */
+public class List {
+    private int subjectAmount;
+
+    public void setSubjectAmount(int subjectAmount) {
+        this.subjectAmount = subjectAmount;
+    }
+
+    public int getSubjectAmount() {
+        return subjectAmount;
+    }
+
+}
