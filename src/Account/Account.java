ded1b44c63e4f146798dfd58b1e925f4cacfee3c
Misha <mishaukr22@gmail.com>
2016-04-16 20:51:46 +0300

    Merge remote-tracking branch 'origin/master'

--- src/Account/Account.java
+++ src/Account/Account.java
@@ -4,4 +4,22 @@ package Account;
  * Created by oleksiypolshchak on 4/15/16.
  */
 public class Account {
+    private int AccountFotoID;
+    private int Data;
+
+    public void setData(int data) {
+        Data = data;
+    }
+
+    public int getData() {
+        return Data;
+    }
+
+    public void setAccountFotoID(int accountFotoID) {
+        AccountFotoID = accountFotoID;
+    }
+
+    public int getAccountFotoID() {
+        return AccountFotoID;
+    }
 }
