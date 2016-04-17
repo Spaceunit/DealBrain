66bab6cad22a886058d748955af5f47c836eb929
yaroslav <yaroslav.mavliutov@gmail.com>
2016-04-17 22:29:31 +0300

    class Content

--- src/Media/Content.java
+++ src/Media/Content.java
@@ -8,6 +8,9 @@ public class Content {
     private long textID;
     private long attachmentID;
 
+    private Media media = new Media();
+    private Text text = new Text();
+
     public void setText(long textID) {this.textID = textID;}
     public long setText() {return textID;}
 
