75163aefa49bd21a1e99084556606f62ee092c82
Oleksiy <oleksiypolshchak@gmail.com>
2016-04-17 20:53:04 +0300

    changes-in-class-Dialog-package-Correspondance

--- src/Correspondence/Dialog.java
+++ src/Correspondence/Dialog.java
@@ -1,11 +1,13 @@
 package Correspondence;
 
+import Account.ID;
+
 /**
  * Created by Misha on 15.04.2016.
  */
 public class Dialog {
-    private int membersID;
-    private int owner;
+    private ID membersID = new ID();
+    private ID owner = new ID();
 
     public void setMembers(int members) {
         this.membersID = members;
