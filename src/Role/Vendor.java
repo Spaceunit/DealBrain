7cb221dc68cdff014088b6cb1fd2bc6529509309
yaroslav <yaroslav.mavliutov@gmail.com>
2016-04-17 22:13:14 +0300

    class Vendor

--- src/Role/Vendor.java
+++ src/Role/Vendor.java
@@ -1,5 +1,7 @@
 package Role;
 
+import java.util.ArrayList;
+
 /**
  * Created by lenovo Nazar on 15.04.2016.
  */
@@ -8,8 +10,7 @@ public class Vendor extends Role{
     String [] all_products;
     private int index_numb;
 
-    private PriceRange pricerange = new PriceRange();
-    private Product product = new Product();
+    private ArrayList<Product> product = new ArrayList<>();
 
     public String[] getProducts() {
         return all_products;
