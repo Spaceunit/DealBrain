41dbbdd1a014e75691cf2976ed46b87bd5bf2ff8
Oleksiy <oleksiypolshchak@gmail.com>
2016-04-17 20:25:30 +0300

    changes-in-class-Photo-package-Account

--- src/Account/Photo.java
+++ src/Account/Photo.java
@@ -1,9 +1,10 @@
 package Account;
 
+import Media.SFile;
+
 /**
  * Created by oleksiypolshchak on 4/15/16.
  */
-public class Photo {
-    private String name = "photo";
-    private String type = "jpg";
+public class Photo extends SFile{
+
 }
