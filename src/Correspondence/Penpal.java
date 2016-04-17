a6978a7134891aa97fc33427bdd1bb130660689e
yaroslav <yaroslav.mavliutov@gmail.com>
2016-04-17 22:21:33 +0300

    class Penpal

--- src/Correspondence/Penpal.java
+++ src/Correspondence/Penpal.java
@@ -1,7 +1,11 @@
 package Correspondence;
 
+import java.util.List;
 /**
  * Created by Yaroslav on 15.04.2016.
  */
     public class Penpal {
+
+    private List<BlackList> blackList;
+    private List<DialogList> dialogList;
 }
